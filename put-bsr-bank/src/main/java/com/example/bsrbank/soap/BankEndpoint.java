64c5d81b3753df4881a1a184f47b94024d555679
Marcin Jabłoński <jablon1994@gmail.com>
2018-01-19 10:22:26 +0100

    Created SOAP endpoint, not working...

--- put-bsr-bank/src/main/java/com/example/bsrbank/soap/BankEndpoint.java
+++ put-bsr-bank/src/main/java/com/example/bsrbank/soap/BankEndpoint.java
@@ -1,5 +1,8 @@
 package com.example.bsrbank.soap;
 
+import com.bank.types.*;
+import com.example.bsrbank.logic.AccountsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -9,5 +12,47 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 public class BankEndpoint {
     private static final String NAMESPACE_URI = "http://www.bank.com/types";
 
+    @Autowired
+    private AccountsService accountsService;
 
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "historyRequest")
+    @ResponsePayload
+    public HistoryResponseType history(@RequestPayload HistoryRequestType payload) {
+
+        HistoryResponseType historyResponse = new HistoryResponseType();
+        HistoryEntryType etry = new HistoryEntryType();
+        etry.setAmount(1000);
+        etry.setBalance(0);
+        etry.setSource("ABD");
+        etry.setTitle("TEST");
+        historyResponse.getHistoryEntry().add(etry);
+        return historyResponse;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "paymentRequest")
+    @ResponsePayload
+    public OperationResponseType payment(@RequestPayload PaymentRequestType payload) {
+
+        OperationResponseType historyResponse = new OperationResponseType();
+
+        return historyResponse;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "withdrawRequest")
+    @ResponsePayload
+    public OperationResponseType withdraw(@RequestPayload WithdrawRequestType payload) {
+
+        OperationResponseType historyResponse = new OperationResponseType();
+
+        return historyResponse;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "transferRequest")
+    @ResponsePayload
+    public OperationResponseType transfer(@RequestPayload TransferRequestType payload) {
+
+        OperationResponseType historyResponse = new OperationResponseType();
+
+        return historyResponse;
+    }
 }
--- put-bsr-bank/src/main/java/com/example/bsrbank/soap/WebServiceConfig.java
+++ put-bsr-bank/src/main/java/com/example/bsrbank/soap/WebServiceConfig.java
@@ -8,8 +8,11 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
 import org.springframework.ws.wsdl.wsdl11.Wsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
 
 @EnableWs
 @Configuration
@@ -18,13 +21,22 @@ public class WebServiceConfig extends WsConfigurerAdapter {
     public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
         MessageDispatcherServlet servlet = new MessageDispatcherServlet();
         servlet.setApplicationContext(applicationContext);
+        servlet.setTransformWsdlLocations(true);
         return new ServletRegistrationBean(servlet, "/ws/*");
     }
 
     @Bean(name = "bank")
-    public Wsdl11Definition defaultWsdl11Definition() {
-        SimpleWsdl11Definition wsdl11Definition = new SimpleWsdl11Definition();
-        wsdl11Definition.setWsdl(new ClassPathResource("bank.wsdl"));
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema bankSchema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("BankPortType");
+        wsdl11Definition.setLocationUri("/ws");
+        wsdl11Definition.setTargetNamespace("http://www.bank.com/types");
+        wsdl11Definition.setSchema(bankSchema);
         return wsdl11Definition;
+    }
+
+    @Bean
+    public XsdSchema countriesSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("types.xsd"));
     }
 }
